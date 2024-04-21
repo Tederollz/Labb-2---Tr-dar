c746e638107e4c9780090d4521a29b7889cb3edd
tederollz <tederollz@gmsil.com>
2024-04-21 14:52:53 +0200

    Add project files.

---
+++ Labb 2 - Trådar/Car.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_2___Trådar
+{
+    public class Car
+    {
+        public string Name { get; }
+        public int Distance { get; private set; }
+        public int Speed { get; private set; }
+
+        private readonly Random random;
+
+        public Car(string name)
+        {
+            Name = name;
+            Distance = 0;
+            Speed = 120; 
+            random = new Random();
+        }
+
+        public void StartRace()
+        {
+            Console.WriteLine($"{Name} has started the race!");
+
+            while (Distance < 100) 
+            {
+                Move();
+                Thread.Sleep(1000); 
+            }
+
+            Console.WriteLine($"{Name} has finished the race!");
+        }
+
+        private void Move()
+        {
+            Distance += Speed / 3600; 
+            CheckForEvent();
+        }
+
+        private void CheckForEvent()
+        {
+            int chance = random.Next(50); 
+
+            if (chance == 0) 
+            {
+                Console.WriteLine($"{Name} is out of gas! Stopping for refueling...");
+                Thread.Sleep(30000); 
+            }
+            else if (chance <= 1) 
+            {
+                Console.WriteLine($"{Name} has a flat tire! Stopping to change the tire...");
+                Thread.Sleep(20000); 
+            }
+            else if (chance <= 6) 
+            {
+                Console.WriteLine($"{Name} has a bird on the windshield! Stopping to clean the windshield...");
+                Thread.Sleep(10000); 
+            }
+            else if (chance <= 15) 
+            {
+                Console.WriteLine($"{Name} has engine trouble! Speed reduced by 1 km/h.");
+                Speed--; 
+            }
+        }
+    }
+}
---
+++ Labb 2 - Trådar/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Labb_2___Trådar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>
+            {
+                new Car("Car 1"),
+                new Car("Car 2")
+            };
+
+            foreach (var car in cars)
+            {
+                Thread carThread = new Thread(car.StartRace);
+                carThread.Start();
+            }
+
+            string input;
+            do
+            {
+                Console.WriteLine("Type 'status' to get race status or press Enter to continue...");
+                input = Console.ReadLine();
+
+                if (input.ToLower() == "status")
+                {
+                    foreach (var car in cars)
+                    {
+                        Console.WriteLine($"{car.Name} is at {car.Distance} km with a speed of {car.Speed} km/h.");
+                    }
+                }
+
+            } while (input.ToLower() != "exit");
+
+            Console.WriteLine("Race ended.");
+        }
+    }
+}
